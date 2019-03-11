053992d24dea27c7d913887c8a13b81cfda03a40
James Dorr <jamesdorr3@gmail.com>
2019-03-10 23:56:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is James"
+  end
+  get '/hometown' do
+    "My hometown is Spokane, Washington"
+  end
+  get '/favorite-song' do
+    "My favorite song is Anywhere - Rita Ora"
+  end
 end
