d138c0f54dcaea1ed26d20380890bdba80de490d
Nyette <antoinette.lee02@myhunter.cuny.edu>
2019-03-10 22:42:49 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Antoinette"
+  end
+
+  get '/hometown' do
+    "My hometown is Queens, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Moondust' by Jaymes Young"
+  end
+
 end
