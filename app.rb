a28c7481ec2918cd0239e34af452547e842af5ef
brianryu <s.brianryu@gmail.com>
2019-03-11 22:09:38 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+    # Define your GET '/' route below and respond with "Hello, World!"
+    get '/' do
+      "Hello, World!"
+    end
+  
+    get '/name' do 
+      "My name is Brian"
+    end 
+  
+    get '/hometown' do 
+      "My hometown is Cypress"
+    end   
+    
+    get '/favorite-song' do 
+      "My favorite song is 'Buss Down Thotiana'"
+    end 
+  end
+  
