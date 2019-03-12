0da08d5d56df27fc9238c3779665032a0e6af0da
campbelllssoup <matjocampbell@gmail.com>
2019-03-11 20:18:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Matthew Joseph Campbell"
+  end
+
+  get '/hometown' do
+    "My hometown is Camden, Arkansas"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Sleepwalkin' by Better Oblivion Community Center"
+  end
+
+
+
 end
